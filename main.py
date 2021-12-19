bd1e0569b6292118768cfd53e413cdb75c21b2bb
kurumethreestars <ilovebooks0618@gmail.com>
2021-12-20 02:07:11 +0900

    Complete

--- main.py
+++ main.py
@@ -1,33 +1,41 @@
-from time import time
+import schedule
+import time
 import tweepy
 from tweepy import api
 import sys
+import os
+key_file = "Keys.txt"
 
 def getAllKeys():
-    consumer_key = 2
-    consumer_secret = 3
-    access_token = 4
-    access_token_secret = 5
+    with open(key_file) as f:
+        keys = [s.strip() for s in f.readlines()]
+
+    consumer_key = keys[0]
+    consumer_secret = keys[1]
+    access_token = keys[2]
+    access_token_secret = keys[3]
+    print(consumer_key)
     return consumer_key,consumer_secret,access_token,access_token_secret
 
 def get_progress(file_name,char_num):
-    with open(file_name) as f:
+    with open(file_name,'r',encoding="utf-8_sig") as f:
         character_all = f.read()
     num_diff = len(character_all) - char_num
     char_num = len(character_all)
     return char_num, num_diff
 
 if __name__=='__main__':
-    file_name = sys.argv[1]
+    file_name = "C:\\Users\\ilove\\Documents\\research\\shuron\\shuron_2_CharacterRecognition.tex"
     char_num = 0
     num_diff = 0
     consumer_key,consumer_secret,access_token,access_token_secret = getAllKeys()
     auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
     auth.set_access_token(access_token,access_token_secret)
-    api = tweepy.API(auth,file_name,char_num)
+    api = tweepy.API(auth)
+    wait_time = 1200
 
     while True:
         char_num,num_diff = get_progress(file_name,char_num)
-        tweet_content = "十分間の進捗は"+str(num_diff)+"字です"
+        tweet_content = "二十分間の進捗は"+str(num_diff)+"字です."
         api.update_status(tweet_content)
-        time.sleep(600)
+        time.sleep(wait_time)
